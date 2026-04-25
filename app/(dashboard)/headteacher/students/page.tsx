a503fbc7258703a7623e460ea1f94900f1238902
King Kuan <kuanjoeart@gmail.com>
2026-04-25 11:47:43 +0200

    Add files via upload

--- app/(dashboard)/headteacher/students/page.tsx
+++ app/(dashboard)/headteacher/students/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+export const dynamic = 'force-dynamic'
+
 import { useEffect, useState } from 'react'
 import DashboardLayout from '@/components/layout/DashboardLayout'
 import { getActiveAcademicYear, getClasses, createStudent } from '@/lib/firebase/firestore'
